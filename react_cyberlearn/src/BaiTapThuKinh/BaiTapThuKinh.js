a4c026a5c7e65bbc2de900e7083e9d06774306d5
Hiep Hoang <hieph@lotusoutsourcing.com>
2021-06-27 22:35:58 +0700

    react_cyberlearn_basic: lesson 19: Hướng dẫn sử lý sự kiện thay đổi kính sủ dụng state và animation

--- react_cyberlearn/src/BaiTapThuKinh/BaiTapThuKinh.js
+++ react_cyberlearn/src/BaiTapThuKinh/BaiTapThuKinh.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
 import dataGlasses from '../Data/dataGlasses.json';
+import './BaiTapThuKinh.css';
 
 class BaiTapThuKinh extends Component {
 
+    state = {
+        glassesCurrent: {
+            "id": 2,
+            "price": 50,
+            "name": "GUCCI G8759H",
+            "url": "./glassesImage/v2.png",
+            "desc": "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
+        }
+    }
 
     renderGlassesList = () => {
         return dataGlasses.map((glassesItem, index) => {
-            return <img className="ml-2 p-2 border border-width-1" style={{ width: '110px', cursor: 'pointer' }} key={index} src={glassesItem.url}></img>
+            return <img onClick={() => { this.changeGlasses(glassesItem) }} className="ml-2 p-2 border border-width-1" style={{ width: '110px', cursor: 'pointer' }} key={index} src={glassesItem.url}></img>
         });
     }
 
+    changeGlasses = (newGlasses) => {
+        this.setState({
+            glassesCurrent: newGlasses
+        })
+    }
+
     render() {
+
+        const keyFrame = `@keyframes animChangeGlasses${Date.now()} {
+            from {
+              width: 0;
+              transform: rotate(45deg);
+              opacity:0;
+            }
+            to {
+              width: 150px;
+              transform: rotate(0deg);
+              opacity:0.8;
+            }
+          }`;
+
 
         const styleGlasses = {
             width: '150px',
             top: '75px',
             right: '70px',
-            opacity: 0.8
+            opacity: 0.8,
+            transform: 'rotate(0deg)',
+            animation: `animChangeGlasses${Date.now()} 1s`
         }
         const infoGlasses = {
             width: '250px',
@@ -29,6 +61,9 @@ class BaiTapThuKinh extends Component {
         }
         return (
             <div style={{ backgroundImage: 'url(./glassesImage/background.jpg)', backgroundSize: '2000px', minHeight: '2000px' }} >
+                <style>
+                    {keyFrame}
+                </style>
                 <div style={{ backgroundColor: 'rgba(0,0,0, .8)', minHeight: '2000px' }}>
                     <h3 style={{ backgroundColor: 'rgba(0,0,0, .3)' }} className="text-center text-light p-5">
                         TRY GLASSES APP ONLINE
@@ -38,10 +73,10 @@ class BaiTapThuKinh extends Component {
                             <div className="col-6">
                                 <div className="position-relative">
                                     <img className="position-absolute" style={{ width: '250px' }} src="./glassesImage/model.jpg" alt="model.jpg"></img>
-                                    <img style={styleGlasses} className="position-absolute" src="./glassesImage/v1.png"></img>
+                                    <img style={styleGlasses} className="position-absolute glassesStyle" src={this.state.glassesCurrent.url}></img>
                                     <div style={infoGlasses} className="position-relative ">
-                                        <p style={{ color: '#AB82FF', fontSize: '17px' }} className="font-weight-bold ">Tên kính</p>
-                                        <span style={{ fontSize: '13px', fontWeight: '400' }}>Mô tả</span>
+                                        <p style={{ color: '#AB82FF', fontSize: '17px' }} className="font-weight-bold ">{this.state.glassesCurrent.name}</p>
+                                        <span style={{ fontSize: '14px', fontWeight: '400', paddingRight: '5px' }}>{this.state.glassesCurrent.desc}</span>
                                     </div>
                                 </div>
                             </div>
